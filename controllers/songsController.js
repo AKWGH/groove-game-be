438b27cc6699eadf6b60de1fa52e2f756beae9c9
alxjnr <alexrobertjoiner@gmail.com>
2023-03-27 16:27:54 +0100

    add functionality for retrieving songs from the Spotify API

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -2,9 +2,13 @@ const { fetchSongs } = require("../models/songsModel");
 
 const getSongs = (req, res) => {
   const { genre } = req.params;
-  fetchSongs(genre).then((data) => {
-    res.status(200).send(data);
-  });
+  fetchSongs(genre)
+    .then((data) => {
+      res.status(200).send(data); // sends the data to the user
+    })
+    .catch((err) => {
+      res.status(404).send("genre not found"); // iff the genre isnt found, return a 404 to the user
+    });
 };
 
 module.exports = { getSongs };
--- models/songsModel.js
+++ models/songsModel.js
@@ -3,23 +3,29 @@ const artistSeeds = require("../db/artistSeeds");
 const axios = require("axios");
 
 const fetchSongs = (genre) => {
-  return axios
-    .get(
-      `https://api.spotify.com/v1/recommendations?seed_artists=${artistSeeds[genre].artist_ids}&seed_genres=${artistSeeds[genre].genre_names}&seed_tracks=${artistSeeds[genre].track_ids}&limit=5`,
-      {
-        headers: {
-          "Content-Type": "application / json",
-          Authorization: "Bearer " + spotifyToken.token,
-          Host: "api.spotify.com",
-        },
-      }
-    )
-    .then((response) => {
-      return response.data;
-    })
-    .catch((err) => {
-      console.log(err);
+  // will fetch songs from the spotify API
+  const artistNames = Object.keys(artistSeeds); // creating an array of genres hardcoded in artistSeeds.js
+  if (!artistNames.includes(genre)) {
+    return Promise.reject({
+      status: 404,
+      msg: `genre not found`, // if the genre in the req.params does not exist within the array, send a 404 back to the users
     });
+  } else {
+    return axios // a get request to the spotify API
+      .get(
+        `https://api.spotify.com/v1/recommendations?seed_artists=${artistSeeds[genre].artist_ids}&seed_genres=${artistSeeds[genre].genre_names}&seed_tracks=${artistSeeds[genre].track_ids}&limit=20`,
+        {
+          headers: {
+            "Content-Type": "application / json",
+            Authorization: "Bearer " + spotifyToken.token, // the request includes the auth token
+            Host: "api.spotify.com",
+          },
+        }
+      )
+      .then((response) => {
+        return response.data; // returns the response back to the controller
+      });
+  }
 };
 
 module.exports = { fetchSongs };
