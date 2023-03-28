4af93be3a236a4b846c657f4aa01fd3fa18d3d11
alxjnr <alexrobertjoiner@gmail.com>
2023-03-28 09:41:59 +0100

    add usernames as a unique property

--- __tests__/endpoints.test.js
+++ __tests__/endpoints.test.js
@@ -1,24 +1,24 @@
-const request = require('supertest');
-const app = require('../app');
+const request = require("supertest");
+const app = require("../app");
 
-describe('app endpoint tests', () => {
-  describe('POST /api/user', () => {
-    it('should respond with a status 201 created and create a new user', () => {
+describe("app endpoint tests", () => {
+  describe("POST /api/user", () => {
+    it("should respond with a status 201 created and create a new user", () => {
       return request(app)
-        .post('/api/user')
+        .post("/api/user")
         .expect(201)
-        .send({ username: 'andy', name: 'andy', password: 'andy' })
+        .send({ username: "andy", name: "andy", password: "andy" })
         .then(({ body }) => {
-          expect(body.msg).toBe('user created');
+          expect(body.msg).toBe("user created");
         });
     });
-    it('should respond with a status code 401 bad request', () => {
+    it("should respond with a status code 401 bad request", () => {
       return request(app)
-        .post('/api/user')
+        .post("/api/user")
         .expect(401)
-        .send({ name: 'andy' })
+        .send({ name: "andy" })
         .then(({ body }) => {
-          expect(body.msg).toBe('Sorry, please fill out all fields');
+          expect(body.msg).toBe("Sorry, please fill out all fields");
         });
     });
   });
--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 // model for posting users to the database
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: [true, "Please provide name"] },
-  username: { type: String, required: [true, "Please provide username"] },
+  username: {
+    type: String,
+    unique: true, // usernames must be unique to be created
+    required: [true, "Please provide username"],
+  },
   password: {
     type: String,
     required: [true, "Please provide password"],
