b3c7a1ba935fb21007608560959ef8d45ed90b52
Andrew Williams <andrew.k.w@outlook.com>
2023-03-28 10:10:22 +0100

    added some tests for signing up

--- __tests__/endpoints.test.js
+++ __tests__/endpoints.test.js
@@ -2,24 +2,50 @@ const request = require('supertest');
 const { app } = require('../app');
 
 describe('app endpoint tests', () => {
+  describe('testing invalid paths handler', () => {
+    it('should respond with a status 404 and msg', () => {
+      return request(app)
+        .get('/api/invalid-path')
+        .expect(404)
+        .then(({ body }) => {
+          expect(body.msg).toBe('Sorry, path not found');
+        });
+    });
+  });
+
   describe('POST /api/user', () => {
     it('should respond with a status 201 created and create a new user', () => {
       return request(app)
         .post('/api/user')
         .expect(201)
-        .send({ username: 'andy', name: 'andy', password: 'andy' })
+        .send({ username: 'andy', name: 'andy', password: 'password' })
         .then(({ body }) => {
           expect(body.msg).toBe('user created');
         });
     });
-    it('should respond with a status code 401 bad request', () => {
+    it('should respond with a status code 400 user validation failed', () => {
       return request(app)
         .post('/api/user')
-        .expect(401)
+        .expect(400)
         .send({ name: 'andy' })
         .then(({ body }) => {
-          expect(body.msg).toBe('Sorry, please fill out all fields');
+          expect(body.msg).toBe('user validation failed');
         });
     });
+    // it('should respond ? user already exists', () => {
+    //   return request(app)
+    //     .post('/api/user')
+    //     .expect(201)
+    //     .send({ name: 'phil', username: 'philly', password: 'password' })
+    //     .then(() => {
+    //       return request(app)
+    //         .post('/api/user')
+    //         .expect(401)
+    //         .send({ name: 'phil', username: 'philly', password: 'password' })
+    //         .then(({ body }) => {
+    //           expect(body.msg).toBe('user already exists');
+    //         });
+    //     });
+    // });
   });
 });
